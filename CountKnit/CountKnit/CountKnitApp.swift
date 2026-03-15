14f94f7b5e9f8799f7ecf9461204abb341e248f7
Bambu <byrr113@gmail.com>
2026-03-15 18:34:21 +0900

    🚚build: 프로젝트 생성

---
+++ CountKnit/CountKnit/CountKnitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountKnitApp.swift
+//  CountKnit
+//
+//  Created by 변예린 on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct CountKnitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
